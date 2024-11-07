2bb34efeb6addb3dc276529a6f19b24f09bf0f08
MauricioSA5401 <134406537+MauricioSA5401@users.noreply.github.com>
2024-11-07 13:03:53 -0600

    Update conexion.php

--- ecommerce/php/conexion.php
+++ ecommerce/php/conexion.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$db = 'ecommerce';
-$user = 'root';
-$password = '';
+$host = 'localhost'; //
+$db = 'ecommerce';//
+$user = 'root';//
+$password = '';//
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $password);
